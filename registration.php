6c164c041971290d17183ce2d8791dff825d1b72
moneer <moneer@octedia.com>
2025-09-24 13:04:12 +0300

    first commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Octedia. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Octedia_FixMonologCustomLogFiles',
+    __DIR__
+);
